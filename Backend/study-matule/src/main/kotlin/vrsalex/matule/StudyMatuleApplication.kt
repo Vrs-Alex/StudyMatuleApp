836d244939cbdbb52452e1e3337fe487692232bb
Alex <alexgm0508@gmail.com>
2025-11-17 16:40:47 +0300

    Init

---
+++ Backend/study-matule/src/main/kotlin/vrsalex/matule/StudyMatuleApplication.kt
@@ -0,0 +1,11 @@
+package vrsalex.matule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyMatuleApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyMatuleApplication>(*args)
+}
---
+++ Backend/study-matule/src/test/kotlin/vrsalex/matule/StudyMatuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package vrsalex.matule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyMatuleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
