99e649df7e390d964c7ead6b4b63dc18a58e36b0
Alex <alexgm0508@gmail.com>
2025-11-25 17:22:01 +0300

    Created custom theme

---
+++ Android/Library/uikit/src/main/java/vrsalex/matule/uikit/theme/Color.kt
@@ -0,0 +1,26 @@
+package vrsalex.matule.uikit.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+
+val Accent = Color(0xFF1A6FEE)
+val AccentInactive = Color(0xFFC9D4FB)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Error = Color(0xFF000000)
+val Success = Color(0xFF000000)
+val InputBackground = Color(0xFF000000)
+val InputStroke = Color(0xFF000000)
+val InputIcon = Color(0xFF000000)
+val Placeholder = Color(0xFF000000)
+val Description = Color(0xFF000000)
+val CardStroke = Color(0xFF000000)
+
+val Gradient1_1 = Color(0xFF75D8BC)
+val Gradient1_2 = Color(0xFF5BADCB)
+val Gradient1 = Brush.linearGradient(colors = listOf(Gradient1_1, Gradient1_2))
+
+val Gradient2_1 = Color(0xFFB4D5FF)
+val Gradient2_2 = Color(0xFF60A6FD)
+val Gradient2 = Brush.linearGradient(colors = listOf(Gradient2_1, Gradient2_2))
---
+++ Android/Library/uikit/src/main/java/vrsalex/matule/uikit/theme/TextFonts.kt
@@ -0,0 +1,14 @@
+package vrsalex.matule.uikit.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import vrsalex.matule.uikit.R
+
+val sfProDisplay = FontFamily(
+    Font(R.font.SF_Pro_Display_Regular, FontWeight.Normal),
+    Font(R.font.SF_Pro_Display_Semibold, FontWeight.SemiBold),
+    Font(R.font.SF_Pro_Display_Heavy, FontWeight(800)),
+    Font(R.font.SF_Pro_Display_Medium, FontWeight.Medium),
+    Font(R.font.SF_Pro_Display_Bold, FontWeight.Bold)
+)
