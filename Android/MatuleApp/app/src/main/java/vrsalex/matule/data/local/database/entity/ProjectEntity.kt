273743fb595864b6f9840ff76737bf9df7b377c4
alexg <alexgm0508@gmail.com>
2025-12-25 17:31:16 +0300

    ProjectEntity: added foreign keys

--- Android/MatuleApp/app/src/main/java/vrsalex/matule/data/local/database/entity/ProjectEntity.kt
+++ Android/MatuleApp/app/src/main/java/vrsalex/matule/data/local/database/entity/ProjectEntity.kt
@@ -1,10 +1,26 @@
 package vrsalex.matule.data.local.database.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "project")
+@Entity(tableName = "project",
+    foreignKeys = [
+        ForeignKey(
+            entity = ProjectTypeEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["typeId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = ProjectCategoryEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ProjectEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
